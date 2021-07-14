45d0bdab03357f17c4d5fb1819b468d1adc066d5
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-14 19:54:08 +0530

    Refactored : To generic method and find maximum

--- Generic/src/Generic/Generic.java
+++ Generic/src/Generic/Generic.java
@@ -12,73 +12,40 @@ public class Generic {
 		x1 = sc.nextInt();
 		x2 = sc.nextInt();
 		x3 = sc.nextInt();
-		findIntMax(x1, x2, x3);
+		maximum(x1,x2,x3);
 		System.out.println("Enter 3 float numbers");
 		y1 = sc.nextFloat();
 		y2 = sc.nextFloat();
 		y3 = sc.nextFloat();
-		findFloatMax(y1, y2, y3);
+		maximum(y1,y2,y3);
 		System.out.println("Enter 3 Strings");
 		z1 = sc.next();
 		z2 = sc.next();
 		z3 = sc.next();
-		findStringMax(z1, z2, z3);
+		maximum(z1,z2,z3);
 		sc.close();
 	}
 
 	/*
-	 * Method to compare 3 string values and get the maximum value.
+	 * Generic Method to compare 3 values and get the maximum value.
 	 * 
-	 * @param a1: first value to compare.
+	 * @param a: first value to compare.
 	 * 
-	 * @param a2: second value to compare.
+	 * @param b: second value to compare.
 	 * 
-	 * @param a3: third value to compare.
+	 * @param c: third value to compare.
 	 */
-	private static void findStringMax(String a1, String a2, String a3) {
-		String max = a1;
-		if(a2.compareTo(max)>0)
-			max = a2;
-		if(a3.compareTo(max)>0)
-			max=a3;
-		System.out.println("Maximum value is "+ max);
-		
-	}
+	public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
+		T max = a;
+		if(b.compareTo(max)>0)
+			max=b;
+		if(c.compareTo(max)>0)
+			max=c;
+	printMax(a,b,c, max);
+	return max;
+}
 
-	/*
-	 * Method to compare 3 float values and get the maximum value.
-	 * 
-	 * @param a1: first value to compare.
-	 * 
-	 * @param a2: second value to compare.
-	 * 
-	 * @param a3: third value to compare.
-	 */
-	private static void findFloatMax(Float a1, Float a2, Float a3) {
-		Float max = a1;
-		if (a2.compareTo(max) > 0)
-			max = a2;
-		if (a3.compareTo(max) > 0)
-			max = a3;
-		System.out.println("Maximum value is " + max);
-
-	}
-
-	/*
-	 * Method to compare 3 integer values and get the maximum value.
-	 * 
-	 * @param a1: first value to compare.
-	 * 
-	 * @param a2: second value to compare.
-	 * 
-	 * @param a3: third value to compare.
-	 */
-	private static void findIntMax(Integer a1, Integer a2, Integer a3) {
-		Integer max = a1;
-		if (a2.compareTo(max) > 0)
-			max = a2;
-		if (a3.compareTo(max) > 0)
-			max = a3;
-		System.out.println("Maximum value is " + max);
+	public static <T> void printMax(T x, T y, T z, T max) {
+		System.out.printf("Maximum of %s %s & %s is %s \n", x, y, z, max);
 	}
 }
