c1aba519b16b3ad5c71d63db9f7fcff09dea634f
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-15 11:46:16 +0530

    Refactored : To generic method and find maximum

--- Generic/src/Generic/Generic.java
+++ Generic/src/Generic/Generic.java
@@ -1,29 +1,13 @@
 package Generic;
 
-import java.util.Scanner;
-
 public class Generic {
 	public static void main(String[] args) {
-		Integer x1, x2, x3;
-		Float y1, y2, y3;
-		String z1,z2,z3;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter 3 integer numbers");
-		x1 = sc.nextInt();
-		x2 = sc.nextInt();
-		x3 = sc.nextInt();
+		Integer x1 = 3, x2 = 5, x3 = 9;
 		maximum(x1,x2,x3);
-		System.out.println("Enter 3 float numbers");
-		y1 = sc.nextFloat();
-		y2 = sc.nextFloat();
-		y3 = sc.nextFloat();
+		Float y1 = 6.3f, y2 = 9.9f, y3 = 5.0f;
 		maximum(y1,y2,y3);
-		System.out.println("Enter 3 Strings");
-		z1 = sc.next();
-		z2 = sc.next();
-		z3 = sc.next();
+		String z1 = "Apple", z2 = "Mango", z3 = "Peach";
 		maximum(z1,z2,z3);
-		sc.close();
 	}
 
 	/*
@@ -37,13 +21,13 @@ public class Generic {
 	 */
 	public static <T extends Comparable<T>> T maximum(T a, T b, T c) {
 		T max = a;
-		if(b.compareTo(max)>0)
-			max=b;
-		if(c.compareTo(max)>0)
-			max=c;
-	printMax(a,b,c, max);
-	return max;
-}
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		printMax(a, b, c, max);
+		return max;
+	}
 
 	public static <T> void printMax(T x, T y, T z, T max) {
 		System.out.printf("Maximum of %s %s & %s is %s \n", x, y, z, max);
