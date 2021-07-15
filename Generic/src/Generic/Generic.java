7dd9fe339f73e6aea2f3129bbeb70324e23eaea4
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-15 16:23:27 +0530

    Refactored : To extend more than three parameters

--- Generic/src/Generic/Generic.java
+++ Generic/src/Generic/Generic.java
@@ -1,60 +1,34 @@
 package Generic;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
-public class Generic<T extends Comparable<T>>{
-	T x1,x2,x3;
-	public Generic(T x1, T x2, T x3) {
-		this.x1 = x1;
-		this.x2 = x2;
-		this.x3 = x3;
+public class Generic<T extends Comparable<T>> {
+	T[] elements;
+
+	public Generic(T[] elements) {
+		this.elements = elements;
 	}
+
 	public static void main(String[] args) {
-		Integer x1, x2, x3;
-		Float y1, y2, y3;
-		String z1, z2, z3;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter 3 integer numbers");
-		// getting 3 integer values from user
-		x1 = sc.nextInt();
-		x2 = sc.nextInt();
-		x3 = sc.nextInt();
-		maximum(x1, x2, x3);
-		System.out.println("Enter 3 float numbers");
-		//getting 3 floats values from user
-		y1 = sc.nextFloat();
-		y2 = sc.nextFloat();
-		y3 = sc.nextFloat();
-		maximum(y1, y2, y3);
-		System.out.println("Enter 3 Strings");
-		//getting 3 String values from user
-		z1 = sc.next();
-		z2 = sc.next();
-		z3 = sc.next();
-		maximum(z1, z2, z3);
-		sc.close();
+		Integer[] integerMax = { 5, 90, 555, 26, 55 };
+		Float[] floatMax = { 3.5f, 9.7f, 11.9f, 18.6f, 2.0f };
+		String[] stringMax = { "Apple", "Strawberry", "Peach", "Banana", "Orange" };
+		System.out.println("Maximum value " + maximum(integerMax));
+		System.out.println("Maximum value " + maximum(floatMax));
+		System.out.println("Maximum value " + maximum(stringMax));
 	}
 
 	/*
-	 * Generic Method to compare 3 values and get the maximum value.
+	 * Generic Method to get maximum value using array sort.
 	 * 
-	 * @param x1: first value to compare.
+	 * @param elements.
 	 * 
-	 * @param x2: second value to compare.
-	 * 
-	 * @param x3: third value to compare.
 	 */
-	public static <T extends Comparable<T>> T maximum(T x1, T x2, T x3) {
-		T max = x1;
-		if (x2.compareTo(max) > 0)
-			max = x2;
-		if (x3.compareTo(max) > 0)
-			max = x3;
-		printMax(x1, x2, x3, max);
-		return max;
+	public static <T extends Comparable<T>> T maximum(T[] elements) {
+		Arrays.sort(elements);
+		T maximum = elements[(elements.length) - 1];
+		return maximum;
+
 	}
 
-	public static <T> void printMax(T x, T y, T z, T max) {
-		System.out.printf("Maximum of %s %s & %s is %s \n", x, y, z, max);
-	}
 }
